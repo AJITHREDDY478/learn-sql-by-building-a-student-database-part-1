c8aaca159b78517ce613ac7ed598da6a008a86d1
AJITH REDDY <ajithreddy478@gmail.com>
2024-03-14 17:35:51 +0000

    Save progress: 550.1

--- insert_data.sh
+++ insert_data.sh
@@ -4,4 +4,24 @@ cat courses.csv
 cat courses.csv | while IFS="," read MAJOR COURSE
 do
 done
+do
+  # get major_id
+
+  # if not found
+
+  # insert major
+
+  # get new major_id
+
+  # get course_id
+
+  # if not found
+
+  # insert course
+
+  # get new course_id
+
+  # insert into majors_courses
+
+done
 
